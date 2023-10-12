27c0cfb34ef460031f759bd6003b847841e8c69b
ARTHUR <arthurdantebiztrapp@gmail.com>
2023-10-11 21:56:00 -0300

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import './css/App.css';
+
+
 
 function App() {
+  const [conteudo, setconteudo] = useState(<h2>Carregando..</h2>)
+  
+  async function carregarTodososPersonagens(){
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    }
+
+    const result = await fetch(
+      'https://rickandmortyapi.com/api/character?',
+      requestOptions
+    )
+      .then(response => response.text)
+      .then(result => {return result})
+      .catch(error => console.log('erro aqui', error))
+
+    const personagens = JSON.parse(result)
+
+    return personagens.results
+  }
+
+  async function montarListaPersonagens(){
+    const personagens = await carregarTodososPersonagens()
+
+    return personagens.map(personagem =>
+      <div className='card char' key={personagem.id}>
+
+        <img src= {personagem.image} alt= {personagem.name}>
+        </img>
+
+        <h2>{personagem.name}</h2>
+
+        <div ClassName='char-info'>
+          <span><b>Especie:</b>{personagem.species}</span>
+          <span><b>Genero:</b>{personagem.gender}</span>
+        </div>
+
+        <div ClassName='lista-secundaria'>
+          <b>Participações</b>
+        </div>
+        <h5><b>Status:</b>{personagem.status}</h5>
+      </div>  
+    )
+  }
+  useEffect(() => {
+    async function pegarConteudo() {
+      setconteudo(await montarListaPersonagens())
+    }
+    pegarConteudo()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="cabecalho">
+        <h1>Rick morty API</h1>
+        <h2><a href='/'>Personagens</a></h2>
       </header>
+      <div className='lista-principal'>
+        { conteudo }
+      </div>
     </div>
   );
 }
